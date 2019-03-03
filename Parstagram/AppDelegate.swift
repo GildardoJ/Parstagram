25782c87a9892f4c047d21a36738d5cfc1b4120e
Gildardo Orozco <gorozco@Gildardos-Mac.local>
2019-03-02 22:35:20 -0800

    finished the videos

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Parstagram"
+                configuration.server = "https://polar-wave-65668.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
